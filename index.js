2b2c84768fbfc7df6e2f35e27474de0ab52257f9
PabloSanturio <pablosanturio1@gmail.com>
2023-07-12 04:59:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+/* 
+let count = 5 + 7
+
+function increment() {
+    
+}
+ */
+
+
+function Number42() {
+    console.log(42);
+}
+
+Number42()
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+// Create a function that logs out the sum of all the lap times
+
+function totalLapTimes(lap1, lap2, lap3) {
+    
+    const totalTimes = lap1 + lap2 + lap3
+    console.log(totalTimes); 
+}
+
+totalLapTimes(lap1, lap2, lap3)
+
+
+let lapsCompleted = 0
+
+// Create a function that increments the lapsCompleted variable with one
+// Run it three times
+
+function increments(laps) {
+   total = laps + 1
+   console.log(total);
+   return total
+} 
+
+increments(lapsCompleted)
+increments(lapsCompleted)
+increments(lapsCompleted)
